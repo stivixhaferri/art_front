c7d2c693c75849ccd0a1796bcfa43ab265b2225c
StiviXhaferri <stivixhaferri01@gmail.com>
2024-07-29 21:47:18 +0200

    art client id

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -43,7 +43,7 @@ export default function RootLayout({ children }) {
         <div className="pt-[8%] bg-[#F7F7F7]  min-h-[90vh]">{children}</div>
         <Bottom/>
         <Toaster />
-        <script src="https://www.paypal.com/sdk/js?client-id=AREeG_yknX_zw5tXJ527HXbLSrx3IBfuqr5jUa6hgPM22mHZjPxW8IqSckhM3HMe539Qi2dwsfgjjI7w" async></script>
+        <script src="https://www.paypal.com/sdk/js?client-id=Af5oAH4IGvBf74g0OZxQjIu03shlNAehWefT7bcnzSKLbI2C51-808rOsjy71-Dspj4yNJwjOLG7eI6u" async></script>
         </body>
     </html>
   );
