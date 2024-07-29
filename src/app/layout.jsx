428a7e4d22de02b3024eb68f62d2c227498f1802
StiviXhaferri <stivixhaferri01@gmail.com>
2024-07-29 20:10:51 +0200

    metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,9 +9,25 @@ import { Toaster } from "@/components/ui/toaster"
 
 export const metadata = {
   title: "Albanian Rental Tourism",
-   description: "Një platformë inovative ku shitësit ofrojnë makinat e tyre për qira dhe klientët mund të rezervojnë dhe paguajnë lehtësisht. Eksploroni ofertat më të mira për makinat që përshtaten me nevojat tuaja dhe bëni një rezervim të shpejtë dhe të sigurt. Zbuloni një përvojë të jashtëzakonshme në turizmin me qira në Shqipëri dhe shijoni udhëtime të paharrueshme me makinat që dëshironi."
+  description: "Një platformë inovative ku shitësit ofrojnë makinat e tyre për qira dhe klientët mund të rezervojnë dhe paguajnë lehtësisht. Eksploroni ofertat më të mira për makinat që përshtaten me nevojat tuaja dhe bëni një rezervim të shpejtë dhe të sigurt. Zbuloni një përvojë të jashtëzakonshme në turizmin me qira në Shqipëri dhe shijoni udhëtime të paharrueshme me makinat që dëshironi.",
+  keywords: "qira makina Shqipëri, turizëm me qira, rezervim makine, platformë qiraje, udhëtime Shqipëri",
+  author: "Albanian Rental Tourism Team",
+  robots: "index, follow",
+  og: {
+    title: "Albanian Rental Tourism",
+    description: "Një platformë inovative për qira makinash në Shqipëri. Rezervoni dhe paguani lehtësisht për makinat që përshtaten me nevojat tuaja dhe shijoni udhëtime të paharrueshme.",
+    image: "https://yourwebsite.com/path-to-your-image.jpg",
+    url: "https://yourwebsite.com",
+    site_name: "Albanian Rental Tourism"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Albanian Rental Tourism",
+    description: "Një platformë inovative për qira makinash në Shqipëri. Rezervoni dhe paguani lehtësisht për makinat që përshtaten me nevojat tuaja dhe shijoni udhëtime të paharrueshme.",
+    image: "https://yourwebsite.com/path-to-your-image.jpg"
+  },
+  favicon: "/logo.png" // Make sure to replace this with the actual path to your favicon file
 };
-
 export default function RootLayout({ children }) {
 
 
