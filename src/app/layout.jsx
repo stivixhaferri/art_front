c033be262830592f73f030130356fe003868dc7b
StiviXhaferri <stivixhaferri01@gmail.com>
2024-08-01 09:37:59 +0200

    faviocn url

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -24,7 +24,7 @@ export const metadata = {
     description: "Një platformë inovative për qira makinash në Shqipëri. Rezervoni dhe paguani lehtësisht për makinat që përshtaten me nevojat tuaja dhe shijoni udhëtime të paharrueshme.",
     
   },
-  favicon: "/favicon.ico" 
+  favicon: "https://albaniarentaltourism.com/favicon.ico" 
 };
 export default function RootLayout({ children }) {
 
